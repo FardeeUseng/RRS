2e0cf3ecc721d322a3d7ad117b982650ec181cef
FardeeTat <fardee.tat@gmail.com>
2021-12-20 00:33:44 +0700

    third commit

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,6 @@ Route::get('/bookdetail',function(){
     return view('bookdetail');
 });
 
-Route::get('/login',function(){
-    return view('login');
+Route::get('/register',function(){
+    return view('register');
 });
